5e4e7f61246617770ceb4f3ceaf4a70be522d6fb
Marcelo Autran <mrautran@gmail.com>
2018-09-30 14:45:52 -0700

    Added React and babel dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
   output: {
     path: __dirname,
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
