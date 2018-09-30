22c15b6870e04e44c83eaf9ec906afeb77f74a4c
Marcelo Autran <mrautran@gmail.com>
2018-09-30 14:46:05 -0700

    Added simple stylesheet and css loader configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './styles.css'
+
 const App = () => {
   return (
-    <div id='react-app'>
+    <div class='heading' id='react-app'>
       My React Boilerplate
     </div>
   )
